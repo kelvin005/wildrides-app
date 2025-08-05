0beb0e18b390726f5aaa2c3c744415e75fbd98ed
Kelechi <63166671+kelvin005@users.noreply.github.com>
2025-08-05 08:22:13 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-2_ABMzxs2Wy', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2vhu1vh3ipoput35ejpjkqke93', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '2vhu1vh3ipoput35ejpjkqke93' , // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://uj2u0kfcpk.execute-api.us-east-2.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
